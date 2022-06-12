7a8f35ae3b0b5442053b1379f897fe4476d988d0
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-12 10:51:54 -0500

    Fix double iteration

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,6 +14,7 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
 	int temp;
+	int flag;
 
 	if (size <= 1)
 		return;
@@ -23,14 +24,21 @@ void selection_sort(int *array, size_t size)
 		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] <= array[min_index])
+			if (array[j] < array[min_index])
 			{
 				min_index = j;
+				flag = 1;
 			}
 		}
-		temp = array[min_index];
-		array[min_index] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		if (flag == 1)
+		{
+			printf("Iteracion\n");
+			temp = array[min_index];
+			array[min_index] = array[i];
+			array[i] = temp;
+			if (i + 2 != size)
+				print_array(array, size);
+			flag = 0;
+		}
 	}
 }
