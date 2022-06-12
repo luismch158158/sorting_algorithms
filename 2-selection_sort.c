0eea61d59e2eb2ac1221838af9313b4a44bc8f6a
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-12 10:00:02 -0500

    Fix condition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,7 +23,7 @@ void selection_sort(int *array, size_t size)
 		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
+			if (array[j] <= array[min_index])
 			{
 				min_index = j;
 			}
