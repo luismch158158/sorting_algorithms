bddb7cf0fd54a0c0b5b585a4c0ac8a1f609c82d5
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-12 09:57:18 -0500

    Adding first condition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,10 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int temp;
 
-	for (i = 0; i < size; i++)
+	if (size <= 1)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
 		for (j = i + 1; j < size; j++)
