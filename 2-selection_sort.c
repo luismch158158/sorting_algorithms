9091923cb0cf65a235845e259290b32e51d050da
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-12 09:41:01 -0500

    Fix size in algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min_index = i;
 		for (j = i + 1; j < size; j++)
